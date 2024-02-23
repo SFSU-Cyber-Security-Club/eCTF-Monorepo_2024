342b8232ab6ad7f9a766bb105f17d77c9625fa8c
Ethan Hanlon <me@ethan-hanlon.xyz>
2024-02-23 13:52:02 -0800

    Fix poetry, gitignore msdk

--- misc_application/shell.nix
+++ misc_application/shell.nix
@@ -25,7 +25,7 @@ pkgs.mkShell {
     pkgs.gcc-arm-embedded
     pkgs.poetry
     pkgs.cacert
-    (pkgs.callPackage custom_nix_pkgs/analog_openocd.nix { })
+    # (pkgs.callPackage custom_nix_pkgs/analog_openocd.nix { })
     pkgs.minicom
   ];
 
