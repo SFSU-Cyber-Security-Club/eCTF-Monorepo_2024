c8628fab5b3352abdd971825a351dd8640abb907
devilmanCr0 <101080560+devilmanCr0@users.noreply.github.com>
2024-02-22 21:54:44 -0800

    Placed all files for building under a subdirectory misc-application

---
+++ misc_application/application_processor/build/simple_crypto.d
@@ -0,0 +1,2 @@
+/home/bristopherwoods/Desktop/CTFs/MITRE/eCTF-Monorepo_2024/application_processor/build/simple_crypto.o: \
+ src/simple_crypto.c
---
+++ misc_application/application_processor/build/startup_firmware.d
@@ -0,0 +1,2 @@
+/home/bristopherwoods/Desktop/CTFs/MITRE/eCTF-Monorepo_2024/application_processor/build/startup_firmware.o: \
+ startup_firmware.S
