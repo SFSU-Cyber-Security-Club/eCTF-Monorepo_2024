77e26e4f7c43bd205c7718dc32823b4b185df775
devilmanCr0 <101080560+devilmanCr0@users.noreply.github.com>
2024-03-18 22:19:34 -0700

    whatever

--- component/inc/ectf_params.h
+++ component/inc/ectf_params.h
@@ -1,8 +1,8 @@
 #ifndef __ECTF_PARAMS__
 #define __ECTF_PARAMS__
 #define COMPONENT_ID 286331153
-#define COMPONENT_BOOT_MSG "booted"
-#define ATTESTATION_LOC "NewYork"
-#define ATTESTATION_DATE "1999"
-#define ATTESTATION_CUSTOMER "JohnBravo"
+#define COMPONENT_BOOT_MSG "component one"
+#define ATTESTATION_LOC "location-attest"
+#define ATTESTATION_DATE "date-attest"
+#define ATTESTATION_CUSTOMER "customer-attest"
 #endif
